24d92f8eb6d4b813551c7a95fe7ceba0dd2e881d
laurelie-pie <jpjosiah@pm.me>
2026-03-23 20:25:51 +0000

    2: All but the asteroids

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 from constants import *
 import pygame
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    player  = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
 
 if __name__ == "__main__":
     main()
